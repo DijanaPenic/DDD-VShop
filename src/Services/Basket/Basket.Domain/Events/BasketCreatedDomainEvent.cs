f7e3da8570f24555aede86819330d6d09e3061c6
Dijana Penić <penic.dijana@gmail.com>
2021-10-13 20:39:15 +0200

    BasketCreatedDomainEvent model cleanup.

--- src/Services/Basket/Basket.Domain/Events/BasketCreatedDomainEvent.cs
+++ src/Services/Basket/Basket.Domain/Events/BasketCreatedDomainEvent.cs
@@ -8,16 +8,6 @@ namespace VShop.Services.Basket.Domain.Events
     {
         public Guid BasketId { get; init; }
         public Guid CustomerId { get; init; }
-        public BasketItem[] BasketItems { get; init; }
         public int CustomerDiscount { get; init; }
-        
-        // TODO - check if this needs to be removed
-        public class BasketItem
-        {
-            public Guid ProductId { get; init; }
-            public string Quantity { get; init; }
-            public decimal UnitPrice { get; init; }
-            public int Discount { get; init; }
-        }
     }
 }
