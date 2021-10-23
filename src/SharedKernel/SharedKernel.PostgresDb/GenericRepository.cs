8fcfc5a8489b676f1b48fc4d82fddb156bfe9f3d
Dijana Penić <penic.dijana@gmail.com>
2021-10-23 17:15:34 +0200

    Implemented GenericRepository class.

---
+++ src/SharedKernel/SharedKernel.PostgresDb/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace VShop.SharedKernel.PostgresDb
+{
+    public class GenericRepository
+    {
+        private readonly ApplicationDbContextBase _dbContext;
+        
+        public GenericRepository(ApplicationDbContextBase dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public Task AddAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            
+            return Task.CompletedTask;
+        }
+        
+        public async Task DeleteByKeyAsync<TEntity>(params object[] keyValues) where TEntity : class
+        {
+            TEntity entity = await _dbContext.Set<TEntity>().FindAsync(keyValues);
+
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+        
+        public Task DeleteAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            
+            return Task.CompletedTask;
+        }
+        
+        public Task UpdateAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/SharedKernel/SharedKernel.PostgresDb/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace VShop.SharedKernel.PostgresDb
+{
+    public interface IGenericRepository
+    {
+        Task AddAsync<TEntity>(TEntity entity) where TEntity : class;
+        Task DeleteByKeyAsync<TEntity>(params object[] keyValues) where TEntity : class;
+        Task DeleteAsync<TEntity>(TEntity entity) where TEntity : class;
+        Task UpdateAsync<TEntity>(TEntity entity) where TEntity : class;
+    }
+}
