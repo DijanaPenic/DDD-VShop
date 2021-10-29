a42ff2425b4174bb10ceb0414fb1ae06c76a3d17
Dijana Penić <penic.dijana@gmail.com>
2021-10-29 19:14:14 +0200

    Added a "work in progress" logic to the ProcessManager class - to handle shopping cart checkout.

--- src/Modules/Sales/Sales.API/Application/DomainEventHandlers/ShoppingCartCheckoutRequested/PlaceOrderDomainEventHandler.cs
+++ src/Modules/Sales/Sales.API/Application/DomainEventHandlers/ShoppingCartCheckoutRequested/PlaceOrderDomainEventHandler.cs
@@ -1,29 +1,7 @@
-﻿using Serilog;
-using System.Threading;
-using System.Threading.Tasks;
-
-using VShop.Modules.Sales.Domain.Events;
-using VShop.SharedKernel.Application.Events;
-
-namespace VShop.Modules.Sales.API.Application.DomainEventHandlers
+﻿namespace VShop.Modules.Sales.API.Application.DomainEventHandlers
 {
-    public class PlaceOrderDomainEventHandler : IDomainEventHandler<ShoppingCartCheckoutRequestedDomainEvent>
+    public class PlaceOrderDomainEventHandler 
     {
-        private readonly ILogger _logger;
 
-        public PlaceOrderDomainEventHandler
-        (
-            ILogger logger
-        )
-        {
-            _logger = logger;
-        }
-        
-        public Task Handle(ShoppingCartCheckoutRequestedDomainEvent @event, CancellationToken cancellationToken)
-        {
-            _logger.Information("Handling domain event: {DomainEvent}", nameof(ShoppingCartCheckoutRequestedDomainEvent));
-            
-            return Task.CompletedTask;
-        }
     }
 }
--- src/Modules/Sales/Sales.API/Application/ProcessManagers/OrderFulfillmentProcessManager.cs
+++ src/Modules/Sales/Sales.API/Application/ProcessManagers/OrderFulfillmentProcessManager.cs
@@ -2,31 +2,46 @@
 using System.Threading;
 using System.Threading.Tasks;
 
+using VShop.Modules.Sales.Domain.Enums;
 using VShop.Modules.Sales.Domain.Events;
 using VShop.Modules.Sales.Infrastructure;
+using VShop.Modules.Sales.Infrastructure.Entities;
+using VShop.SharedKernel.Application.Events;
+using VShop.SharedKernel.Application.Commands;
 
-namespace VShop.Modules.Sales.Application.ProcessManagers
+namespace VShop.Modules.Sales.API.Application.ProcessManagers
 {
-    public class OrderFulfillmentProcessManager
+    public class OrderFulfillmentProcessManager :
+        IDomainEventHandler<ShoppingCartCheckoutRequestedDomainEvent>
     {
         private readonly ILogger _logger;
         private readonly SalesContext _dbContext;
+        private readonly ICommandBus _commandBus;
         
         public OrderFulfillmentProcessManager
         (
             ILogger logger,
+            ICommandBus commandBus,
             SalesContext dbContext
         )
         {
             _logger = logger;
+            _commandBus = commandBus;
             _dbContext = dbContext;
         }
         
-        public Task Handle(ShoppingCartCheckoutRequestedDomainEvent @event, CancellationToken cancellationToken)
+        public async Task Handle(ShoppingCartCheckoutRequestedDomainEvent @event, CancellationToken cancellationToken)
         {
             _logger.Information("Handling domain event: {DomainEvent}", nameof(ShoppingCartCheckoutRequestedDomainEvent));
             
-            return Task.CompletedTask;
+            _dbContext.OrderFulfillmentProcesses.Add(new OrderFulfillmentProcess
+            {
+                OrderId = @event.ShoppingCartId, // TODO - need to handler OrderId mapping instead
+                Status = OrderFulfillmentStatus.CheckoutRequested
+            });
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            //_commandBus.Send() // TODO - need to trigger order creation; need to return command?
         }
     }
 }
