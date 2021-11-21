bf8aa311bd48e50d4f9aa3ee38474fc737f41e0d
Dijana Penić <penic.dijana@gmail.com>
2021-11-21 15:37:54 +0100

    Billing - added migrations for the payment table.

--- src/Modules/Billing/Billing.API/Infrastructure/Extensions/PostgresExtensions.cs
+++ src/Modules/Billing/Billing.API/Infrastructure/Extensions/PostgresExtensions.cs
@@ -10,12 +10,12 @@ namespace VShop.Modules.Billing.API.Infrastructure.Extensions
         public static void AddPostgresServices(this IServiceCollection services, string connectionString)
         {
             services.AddDbContext<BillingContext>(options => options.UseNpgsql
-                (
-                    connectionString,
-                    // Target project needs to match migrations assembly
-                    optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(Startup).Assembly.GetName().Name)
-                )
-                .UseSnakeCaseNamingConvention());
+            (
+                connectionString,
+                // Target project needs to match migrations assembly
+                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(Startup).Assembly.GetName().Name)
+            )
+            .UseSnakeCaseNamingConvention());
         }
     }
 }
