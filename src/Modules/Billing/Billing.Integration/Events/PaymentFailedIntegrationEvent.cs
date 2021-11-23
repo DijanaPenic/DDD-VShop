f2e245895c7ffa7b28a5dfd814dabadedeceb796
dijana.penic <penic.dijana@gmail.com>
2021-11-23 18:18:31 +0100

    Added payment integration events.

---
+++ src/Modules/Billing/Billing.Integration/Events/PaymentFailedIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using VShop.SharedKernel.Messaging.Events;
+
+namespace VShop.Modules.Billing.Integration.Events
+{
+    public record PaymentFailedIntegrationEvent : IntegrationEvent
+    {
+        public Guid OrderId { get; }
+
+        public PaymentFailedIntegrationEvent(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
---
+++ src/Modules/Billing/Billing.Integration/Events/PaymentSucceededIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using VShop.SharedKernel.Messaging.Events;
+
+namespace VShop.Modules.Billing.Integration.Events
+{
+    public record PaymentSucceededIntegrationEvent : IntegrationEvent
+    {
+        public Guid OrderId { get; }
+        
+        public PaymentSucceededIntegrationEvent(Guid orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
