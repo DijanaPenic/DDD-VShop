b513053a1c3027054ddf08c13f053bcb6f451178
dijana.penic <penic.dijana@gmail.com>
2021-11-26 13:25:25 +0100

    Commented out code for ES checkpoint repository.

--- src/SharedKernel/SharedKernel.EventStoreDb.Subscriptions/Repositories/Contracts/ICheckpointRepository.cs
+++ src/SharedKernel/SharedKernel.EventStoreDb.Subscriptions/Repositories/Contracts/ICheckpointRepository.cs
@@ -1,11 +1,11 @@
-﻿using System.Threading;
-using System.Threading.Tasks;
-
-namespace VShop.SharedKernel.EventStoreDb.Subscriptions.Repositories.Contracts
-{
-    public interface ICheckpointRepository
-    {
-        ValueTask<ulong?> LoadAsync(string subscriptionId, CancellationToken cancellationToken);
-        ValueTask SaveAsync(string subscriptionId, ulong position, CancellationToken cancellationToken);
-    }
-}
+﻿// using System.Threading;
+// using System.Threading.Tasks;
+//
+// namespace VShop.SharedKernel.EventStoreDb.Subscriptions.Repositories.Contracts
+// {
+//     public interface ICheckpointRepository
+//     {
+//         ValueTask<ulong?> LoadAsync(string subscriptionId, CancellationToken cancellationToken);
+//         ValueTask SaveAsync(string subscriptionId, ulong position, CancellationToken cancellationToken);
+//     }
+// }
