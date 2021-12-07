dcccd8d04e4eb3b219b0d9259b87cd8e3e01ae2f
dijana.penic <penic.dijana@gmail.com>
2021-12-07 13:00:17 +0100

    Added FixtureExtensions class to the SharedKernel.Infrastructure project.

---
+++ src/SharedKernel/SharedKernel.Infrastructure/Extensions/FixtureExtensions.cs
@@ -0,0 +1,13 @@
+using AutoFixture;
+
+namespace VShop.SharedKernel.Infrastructure.Extensions
+{
+    public static class FixtureExtensions
+    {
+        public static int CreateInt(this IFixture fixture, int min, int max) 
+            => fixture.Create<int>() % (max - min + 1) + min;
+        
+        public static decimal CreateDecimal(this IFixture fixture, decimal min, decimal max) 
+            => fixture.Create<decimal>() % (max - min + 1) + min;
+    }
+}
