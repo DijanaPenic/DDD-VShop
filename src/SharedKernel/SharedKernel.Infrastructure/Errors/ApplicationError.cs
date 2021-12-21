c97131d7fecaf6b4577093117f24de33cadd1835
dijana.penic <penic.dijana@gmail.com>
2021-12-21 12:06:33 +0100

    ApplicationError - added ToString override.

--- src/SharedKernel/SharedKernel.Infrastructure/Errors/ApplicationError.cs
+++ src/SharedKernel/SharedKernel.Infrastructure/Errors/ApplicationError.cs
@@ -4,5 +4,8 @@ namespace VShop.SharedKernel.Infrastructure.Errors
 {
     [GenerateOneOf]
     public partial class ApplicationError : OneOfBase
-        <ValidationError, InternalServerError, NotFoundError> { }
+        <ValidationError, InternalServerError, NotFoundError>
+    {
+        public override string ToString() => Value.ToString();
+    }
 }
