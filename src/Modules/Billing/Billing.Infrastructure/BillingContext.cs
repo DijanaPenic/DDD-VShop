334a3932d475f5fd84c9241f046a95ed21b702e6
dijana.penic <penic.dijana@gmail.com>
2021-12-23 12:20:18 +0100

    BillingContext - fixed instance initialization; it was missing a call to the base class.

--- src/Modules/Billing/Billing.Infrastructure/BillingContext.cs
+++ src/Modules/Billing/Billing.Infrastructure/BillingContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 using VShop.SharedKernel.PostgresDb;
+using VShop.SharedKernel.Infrastructure.Services.Contracts;
 using VShop.Modules.Billing.Infrastructure.Entities;
 using VShop.Modules.Billing.Infrastructure.EntityConfigurations;
 
@@ -8,17 +9,15 @@ namespace VShop.Modules.Billing.Infrastructure
 {
     public class BillingContext : DbContextBase
     {
-        private readonly IDbContextBuilder _contextBuilder;
-        
         public const string PaymentSchema = "payment";
 
         public DbSet<PaymentTransfer> Payments { get; set; }
 
-        public BillingContext(IDbContextBuilder contextBuilder)
-            => _contextBuilder = contextBuilder;
+        public BillingContext(IClockService clockService, IDbContextBuilder contextBuilder) 
+            : base(clockService, contextBuilder) { }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => _contextBuilder.ConfigureContext(optionsBuilder);
+            => ContextBuilder.ConfigureContext(optionsBuilder);
         
         protected override void OnModelCreating(ModelBuilder builder)
             => builder.ApplyConfiguration(new PaymentEntityTypeConfiguration());
--- src/SharedKernel/SharedKernel.Infrastructure/Services/Contracts/IClockService.cs
+++ src/SharedKernel/SharedKernel.Infrastructure/Services/Contracts/IClockService.cs
@@ -5,13 +5,9 @@ namespace VShop.SharedKernel.Infrastructure.Services.Contracts
     public interface IClockService
     {
         DateTimeZone TimeZone { get; }
-
         Instant Now { get; }
-
         LocalDateTime LocalNow { get; }
-
         Instant ToInstant(LocalDateTime local);
-
         LocalDateTime ToLocal(Instant instant);
     }
 }
