a3e5949aa93090a086198df115d779d9a679fc75
dijana.penic <penic.dijana@gmail.com>
2022-01-12 17:46:38 +0100

    Created migration UsedByteMessageRepresentation - sales bc.

--- src/Modules/Sales/Sales.API/Infrastructure/SchedulerMigrations/20220112164356_UsedByteMessageRepresentation.Designer.cs
+++ src/Modules/Sales/Sales.API/Infrastructure/SchedulerMigrations/20220112164356_UsedByteMessageRepresentation.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NodaTime;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,10 @@ using VShop.SharedKernel.Scheduler.Infrastructure;
 namespace VShop.Modules.Sales.API.Infrastructure.SchedulerMigrations
 {
     [DbContext(typeof(SchedulerContext))]
-    partial class SchedulerContextModelSnapshot : ModelSnapshot
+    [Migration("20220112164356_UsedByteMessageRepresentation")]
+    partial class UsedByteMessageRepresentation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Modules/Sales/Sales.API/Infrastructure/SchedulerMigrations/20220112164356_UsedByteMessageRepresentation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.Modules.Sales.API.Infrastructure.SchedulerMigrations
+{
+    public partial class UsedByteMessageRepresentation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "body",
+                schema: "scheduler",
+                table: "message_log",
+                type: "bytea",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "metadata",
+                schema: "scheduler",
+                table: "message_log",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "metadata",
+                schema: "scheduler",
+                table: "message_log");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "body",
+                schema: "scheduler",
+                table: "message_log",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "bytea");
+        }
+    }
+}
